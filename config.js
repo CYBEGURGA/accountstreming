1c9cbb2c65d4bae20f3f6d85c0a71f2e06308f6e
CYBEGURGA <65920521+CYBEGURGA@users.noreply.github.com>
2022-09-10 02:16:13 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+{
+"token": "TOKEN",/// token lera dane
+"id": "id acc",/// id account akay xot lera dane
+"stats": "STREAMING",
+"text": "Create By GURGA", // dastkari awa maka project a tek ache
+"text2": "شتێک بۆ ژێر ناوەکەی", /// awayan dastkari bka ba dle xot shtek dane
+"idvc": "copy id voice aka" /// id voice aka lera dane
+}
